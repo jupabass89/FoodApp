b6b767aba0495d152075eac13cf496492855a814
JDavid <jhndagon@gmail.com>
2017-05-14 16:45:21 -0500

    Separacion de graficas en clase GraficaXY

--- app/src/main/java/com/example/juan/foodapp/controlador/GraficaTanqueActivity.java
+++ app/src/main/java/com/example/juan/foodapp/controlador/GraficaTanqueActivity.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 
 import com.example.juan.foodapp.R;
 import com.example.juan.foodapp.modelo.practicaTanque.PracticaTanque;
+import com.example.juan.foodapp.modelo.representacion.GraficaXY;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
@@ -43,7 +44,7 @@ public class GraficaTanqueActivity extends AppCompatActivity {
         p = new PracticaTanque(this, null, null);
         hacerCalculos();
         line = (LineChart)findViewById(R.id.graficaTanque);
-        graficar();
+        GraficaXY.obtenerGraficaCoeficientes(r,line);
     }
 
     private void hacerCalculos(){
@@ -51,78 +52,14 @@ public class GraficaTanqueActivity extends AppCompatActivity {
         r = p.calcularDatosGrafica();
     }
 
-    private void graficarTiempos(){
-        ArrayList<Float> f = (ArrayList<Float>) r.get(2);
-        ArrayList<Float> g = (ArrayList<Float>) r.get(3);
-        ArrayList<Entry> tiempoCalenTeorico = new ArrayList<>();
-        ArrayList<Entry> tiempoEnfiTeorico = new ArrayList<>();
-        ArrayList<Entry> tiempoCalenExp = new ArrayList<>();
-        ArrayList<Entry> tiempoEnfiExp = new ArrayList<>();
-
-
-        for (int i = 0; i < f.size(); i++) {
-            tiempoCalenExp.add(new Entry(i*60,tempExperiCalenta.get(i)));
-            tiempoCalenTeorico.add(new Entry(f.get(i),tempExperiCalenta.get(i)));
-        }
-        for (int i = 0; i < g.size(); i++) {
-            tiempoEnfiExp.add(new Entry(i*60, tempExperiEnfria.get(i)));
-            tiempoEnfiTeorico.add(new Entry(g.get(i),tempExperiEnfria.get(i)));
-        }
-
-        LineDataSet dataset = new LineDataSet(tiempoCalenExp,"Calent_Experi");
-        dataset.setColor(Color.RED);
-        LineDataSet datasetE = new LineDataSet(tiempoEnfiExp,"Enfria_Experi");
-        datasetE.setColor(Color.rgb(0,0,255));
-
-        LineDataSet datasetF = new LineDataSet(tiempoCalenTeorico,"Calent_Teórico");
-        datasetF.setColor(Color.rgb(252,174,18));
-        LineDataSet datasetG = new LineDataSet(tiempoEnfiTeorico,"Enfria_Teórico");
-        datasetE.setColor(Color.rgb(0,255,0));
-
-        LineData data = new LineData(dataset,datasetE,datasetF,datasetG);
-        line.setMaxVisibleValueCount(1);
-        line.setData(data);
-        line.setBackgroundColor(Color.rgb(245,240,194));
-        line.notifyDataSetChanged();
-        line.invalidate();
-    }
-
-
-    private void graficar(){
-        ArrayList<Float> f = (ArrayList<Float>) r.get(0);
-        ArrayList<Float> g = (ArrayList<Float>) r.get(1);
-        ArrayList<Entry> c = new ArrayList<>();
-        ArrayList<Entry> e = new ArrayList<>();
-
-        for (int i = 0; i < f.size(); i++) {
-            c.add(new Entry((float)i*60, f.get(i)-20f));
-
-        }
-        for (int i = 0; i < g.size(); i++) {
-            e.add(new Entry((float)i*60, g.get(i)-40f));
-        }
-
-        LineDataSet dataset = new LineDataSet(c,"Calentamiento");
-        dataset.setColor(Color.RED);
-        LineDataSet datasetE = new LineDataSet(e,"Enfriamiento");
-        datasetE.setColor(Color.BLUE);
-
-        LineData data = new LineData(dataset,datasetE);
-        line.setMaxVisibleValueCount(1);
-        line.setData(data);
-        line.setBackgroundColor(Color.rgb(245,240,194));
-        line.notifyDataSetChanged();
-        line.invalidate();
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if(id ==R.id.coeficientes){
-            graficar();
+            GraficaXY.obtenerGraficaCoeficientes(r,line);
         }
         else if(id == R.id.tiempo){
-            graficarTiempos();
+            GraficaXY.graficarTiempos(r,tempExperiCalenta,tempExperiEnfria,line);
 
         }
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/example/juan/foodapp/modelo/representacion/GraficaXY.java
+++ app/src/main/java/com/example/juan/foodapp/modelo/representacion/GraficaXY.java
@@ -1,5 +1,77 @@
 package com.example.juan.foodapp.modelo.representacion;
 
+import android.graphics.Color;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+
 public class GraficaXY {
+
+    public static void obtenerGraficaCoeficientes(ArrayList<Object> r,LineChart line){
+        ArrayList<Float> f = (ArrayList<Float>) r.get(0);
+        ArrayList<Float> g = (ArrayList<Float>) r.get(1);
+        ArrayList<Entry> c = new ArrayList<>();
+        ArrayList<Entry> e = new ArrayList<>();
+
+        for (int i = 0; i < f.size(); i++) {
+            c.add(new Entry((float)i*60, f.get(i)-20f));
+
+        }
+        for (int i = 0; i < g.size(); i++) {
+            e.add(new Entry((float)i*60, g.get(i)-40f));
+        }
+
+        LineDataSet dataset = new LineDataSet(c,"Calentamiento");
+        dataset.setColor(Color.RED);
+        LineDataSet datasetE = new LineDataSet(e,"Enfriamiento");
+        datasetE.setColor(Color.BLUE);
+
+        LineData data = new LineData(dataset,datasetE);
+        line.setMaxVisibleValueCount(1);
+        line.setData(data);
+        line.setBackgroundColor(Color.rgb(245,240,194));
+        line.notifyDataSetChanged();
+        line.invalidate();
+    }
+
+    public static void graficarTiempos(ArrayList<Object> r,ArrayList<Float> tempExperiCalenta,ArrayList<Float> tempExperiEnfria,LineChart line){
+        ArrayList<Float> f = (ArrayList<Float>) r.get(2);
+        ArrayList<Float> g = (ArrayList<Float>) r.get(3);
+        ArrayList<Entry> tiempoCalenTeorico = new ArrayList<>();
+        ArrayList<Entry> tiempoEnfiTeorico = new ArrayList<>();
+        ArrayList<Entry> tiempoCalenExp = new ArrayList<>();
+        ArrayList<Entry> tiempoEnfiExp = new ArrayList<>();
+
+
+        for (int i = 0; i < f.size(); i++) {
+            tiempoCalenExp.add(new Entry(i*30,tempExperiCalenta.get(i)));
+            tiempoCalenTeorico.add(new Entry(f.get(i),tempExperiCalenta.get(i)));
+        }
+        for (int i = 0; i < g.size(); i++) {
+            tiempoEnfiExp.add(new Entry(i*60, tempExperiEnfria.get(i)));
+            tiempoEnfiTeorico.add(new Entry(g.get(i),tempExperiEnfria.get(i)));
+        }
+
+        LineDataSet dataset = new LineDataSet(tiempoCalenExp,"Calent_Experi");
+        dataset.setColor(Color.RED);
+        LineDataSet datasetE = new LineDataSet(tiempoEnfiExp,"Enfria_Experi");
+        datasetE.setColor(Color.rgb(0,0,255));
+
+        LineDataSet datasetF = new LineDataSet(tiempoCalenTeorico,"Calent_Teórico");
+        datasetF.setColor(Color.rgb(252,174,18));
+        LineDataSet datasetG = new LineDataSet(tiempoEnfiTeorico,"Enfria_Teórico");
+        datasetE.setColor(Color.rgb(0,255,0));
+
+        LineData data = new LineData(dataset,datasetE,datasetF,datasetG);
+        line.setMaxVisibleValueCount(1);
+        line.setData(data);
+        line.setBackgroundColor(Color.rgb(245,240,194));
+        line.notifyDataSetChanged();
+        line.invalidate();
+    }
     
 }
