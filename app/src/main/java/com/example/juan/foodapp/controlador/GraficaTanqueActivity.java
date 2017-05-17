0400f1472b2168ec091121401a12e35ff9ac257b
santiagoandresmh <santiagoandresmh@gmail.com>
2017-05-17 06:54:44 -0500

    Algunos fullscreens y menus

--- app/src/main/java/com/example/juan/foodapp/controlador/GraficaTanqueActivity.java
+++ app/src/main/java/com/example/juan/foodapp/controlador/GraficaTanqueActivity.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -38,6 +40,8 @@ public class GraficaTanqueActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_grafica_tanque);
         graficaTiempos = false;
         contexto = getApplicationContext();
